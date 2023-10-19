dc7ecba09522b72104f614b90eb6673f4fa0d59c
devuchiha <devvivek956@gmail.com>
2023-10-19 16:28:30 +0630

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoadingComponent } from './loading/loading.component';
+import { DetailsComponent } from './details/details.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoadingComponent,
+    DetailsComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/details/details.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  constructor(private activatedRoute: ActivatedRoute, private apiservice: ApiService) { }
+  movieId: String = '';
+
+  casts = []
+overview: any = {}
+
+  ngOnInit() {
+    this.movieId = this.activatedRoute.snapshot.params['id'];
+    this.apiservice.getCasts(this.movieId).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.casts = data['cast'];
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      }
+
+    })//Observable
+
+
+
+    this.apiservice.getMovieDetails(this.movieId).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.overview = data;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      }
+    })
+
+
+  }
+}
